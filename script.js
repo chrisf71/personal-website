f44df3e435005ab6a0eab348d8e4a065d6894d44
chrisf71 <christopherf442@hotmail.com>
2024-01-01 19:20:29 +0000

    add event listener for avatar

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Change image border color on mouseover
+function changeBorderColor() {
+    const avatar = document.querySelector('.avatar');
+    avatar.style.borderColor = '#F39C12';
+  }
+  
+  function resetBorderColor() {
+    const avatar = document.querySelector('.avatar');
+    avatar.style.borderColor = '';
+  }
+
 // Change background color on mouseover
 const headers = document.querySelectorAll('h2, h3');
 headers.forEach(header => {
